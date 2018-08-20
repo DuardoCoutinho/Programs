a8fa99221c71ed9db39b4a6f901a93c3198803a6
Eduardo Coutinho <josecoutinho2500@gmail.com>
2018-08-20 14:12:41 -0300

    Adicionando formatação e atualizando comparadaor

--- comparador/comparador.py
+++ comparador/comparador.py
@@ -1,37 +1,42 @@
+import formatacao as form
 
-
-txt1 = open('Cria-Usuarios-CSV PRONTO - User.csv')
-txt2 = open('Cria-Usuarios-CSV PRONTO - User (copia).csv')
-texto_oficial = txt1.readlines()
 resul = open('result.txt', 'w')
-
+todo_user = open('todosusuarios.csv','a')
 
 def escrevertxt(txt):
+    
     resul.write(txt)
+    todo_user.write(txt)
+    
+    #print (txt)
+
+
+
+def main():
+    newuser = open('txtformatado.csv').readlines()
+    user = open('todosusuarios.csv').readlines()
     
 
-for txttest in txt2.readlines():
-    
-    cont = 0
-    for txtofic in texto_oficial:
-        
-        
-        cont = 1 + cont
-        if txttest == txtofic:
-            break
-        elif cont == len(texto_oficial):
-            print(txttest)
+    for txttest in newuser:
+        cont = 0
+        if len(user) == 0:
             escrevertxt(txttest)
-
-
-
-
-
+        else:
+            for txtofic in user:
+                cont += 1
+                if cont == len(user):
+                    escrevertxt(txttest)
+                elif str(txtofic) == str(txttest):
+                    
+                    break
             
-
-        
-        
-        
-
                 
-        
+
+
+
+
+
+if __name__=='__main__':
+    form.main()
+    main()
+    
