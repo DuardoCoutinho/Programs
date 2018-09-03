56d147748d416eb0c9f4132b80a5d3136d9bc956
Eduardo Coutinho <Eduardo@escolameta.edu>
2018-09-03 18:18:02 -0300

    lalala

--- comparador/comparador.py
+++ comparador/comparador.py
@@ -4,25 +4,6 @@ import sqlite3
 db = sqlite3.connect('usuarios.db')
 cur = db.cursor()
 
-<<<<<<< HEAD
-#resul = open('result.txt', 'w')
-
-def escrevertxt(txt):
-    print(txt)      
-    sql = ('INSERT INTO user VALUES ('+txt[0]+', "'+txt[1]+'", "'+txt[2]+'", "'+txt[3]+'");')
-    cur.execute(sql)
-    form.newstring(txt[0],txt[1],txt[2])
-    db.commit()
-
-def main():
-    newuser = open('Relatorio.Customizado.GUID.60.csv').readlines()
-    zero = True
-=======
-
-
-resul = open('result.txt', 'w')
-
-
 
 
 def escrevertxt(txt):
@@ -35,45 +16,31 @@ def escrevertxt(txt):
     string = form.newstring(txt[0],txt[1],txt[2])
     db.commit()
     
-    
-
-
 
 def main():
-    newuser = open('novosusuarios.txt').readlines()
-    
-    
-    
-    
-    
->>>>>>> f86889e00e77cb5b2cea3c0c3b80f0d47e49cda9
+    newuser = open('Relatorio.Customizado.GUID.60.csv').readlines()
+    zero = True
+
     for txttest in newuser:
         
         txttest = form.formata(txttest)
-        txttest2 = txttest
-<<<<<<< HEAD
         if zero:
             zero = False
             continue
-        cur.execute("SELECT matricula from user where matricula = "+txttest[0]+"")
+
+        
+        cur.execute('SELECT matricula FROM user WHERE matricula = '+txttest[0]+'')
         user = cur.fetchone()
 
         if  user is None:
-=======
-        
-        txttest = txttest[2]
-        print(txttest)
-        cur.execute("SELECT matricula from user where matricula = "+txttest+"")
-        user = cur.fetchone()
-        if  user is None:
-            print('danone')
->>>>>>> f86889e00e77cb5b2cea3c0c3b80f0d47e49cda9
-            escrevertxt(txttest2)
+               escrevertxt(txttest)
+
+
+            
         
                     
 
 if __name__=='__main__':
-    
     main()
     db.close()
     
--- comparador/formatacao.py
+++ comparador/formatacao.py
@@ -2,11 +2,9 @@ from datetime import datetime
 
 now = datetime.now()
 txt1 = open('novosusuarios.txt') 
-<<<<<<< HEAD
-resul = open('newuseradd.txt', "w")
-=======
+
 resul = open("newuseradd.csv", "w")
->>>>>>> f86889e00e77cb5b2cea3c0c3b80f0d47e49cda9
+
 zero = True
 
 def main():
@@ -16,11 +14,9 @@ def main():
         j = txt.split('\t')
         #newstring(formata(j[0]), formata(j[1]), formata(j[2]))
         
-<<<<<<< HEAD
-def newstring(matri, name, sobname):
-=======
+
 def newstring(name, sobname, matri):
->>>>>>> f86889e00e77cb5b2cea3c0c3b80f0d47e49cda9
+
     global zero
     if zero:
         string = 'DN,objectClass,sAMAccountName,givenName,userPrincipalName,sn'
@@ -28,15 +24,7 @@ def newstring(name, sobname, matri):
     else:
         string = "\n\"CN=" + sobname +",OU=Alunos,DC=FaculdadeMeta,DC=EDU\",user,"+matri+","+name+","+matri+"@faculdademeta.edu,"+sobname
     escrevertxt(string)
-    
-<<<<<<< HEAD
-=======
-    
 
-def escrevertxt(txt):
-    
-    resul.write(txt)
->>>>>>> f86889e00e77cb5b2cea3c0c3b80f0d47e49cda9
     
 
 def escrevertxt(txt):
