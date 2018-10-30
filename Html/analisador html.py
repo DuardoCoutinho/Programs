52707fcc1e154e4e7621eeeea4ef1a4e39971911
Eduardo Coutinho <41974289+DuardoCoutinho@users.noreply.github.com>
2018-10-30 17:53:39 -0300

    Delete analisador html.py

--- Html/analisador html.py
+++
@@ -1,9 +0,0 @@
-
-from bs4 import BeautifulSoup
-import urllib.request
-
-with urllib.request.urlopen('Relatorio de Professores.html') as response:
-   webpage = response.read()
-   soup = BeautifulSoup(webpage,'html.parser')
-   for anchor in soup.find_all('a'):
-       print(anchor.get('href', '/'))
